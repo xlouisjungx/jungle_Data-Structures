983d0302bfcf0087e37bc6e8474f20860e5d0bf4
xlouisjungx <cutaandcrazy@gmail.com>
2025-04-16 07:38:56 +0000

    Solved Q1 in Binary_Tree

--- Binary_Tree/Q1_E_BT.c
+++ Binary_Tree/Q1_E_BT.c
@@ -116,7 +116,11 @@ int main()
 int identical(BTNode *tree1, BTNode *tree2)
 
 {
-   /* add your code here */
+	if(tree1 == NULL && tree2 == NULL) return 1;
+
+	if(tree1 == NULL || tree2 == NULL) return 0;
+	
+	return identical(tree1->left, tree2->left) && identical(tree1->right, tree2->right);
 }
 
 /////////////////////////////////////////////////////////////////////////////////

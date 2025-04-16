21084f858376f2ca7e22211e8326e499db31dad4
xlouisjungx <cutaandcrazy@gmail.com>
2025-04-16 15:15:24 +0000

    Solved Q4 in Binary_Tree

--- Binary_Tree/Q4_E_BT.c
+++ Binary_Tree/Q4_E_BT.c
@@ -103,7 +103,14 @@ int main()
 int sumOfOddNodes(BTNode *node)
 
 {
-    /* add your code here */
+	if(node == NULL) return 0;
+	int left = 0, right = 0;
+
+	left = sumOfOddNodes(node->left);
+	right = sumOfOddNodes(node->right);
+
+	if(node->item % 2 != 0) return left + right + node->item;
+	return left + right;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
